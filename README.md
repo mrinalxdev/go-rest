49b6bed0871ec11f92e84260020e3dfe18e25547
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-01-24 17:54:16 +0530

    Update : Added Data

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is QuizVerse Beta App for the ultimate Quizing
--- app.js
+++ app.js
@@ -56,3 +56,36 @@ const quizData = [
         correct : 'd'
     }
 ]
+
+const questionEl = document.getElementById('question') 
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text') 
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitEl = document.getElementById('submit')
+
+let currentQuiz = 0 
+loadQuiz()
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerText = currentQuizData.question
+
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+
+}
+
+submitEl.addEventListener('click', () => {
+    currentQuiz ++
+
+    if (currentQuiz < quizData.length) {
+        loadQuiz()
+    }else {
+        alert("You Finished")
+    }
+
+    loadQuiz()
+})
