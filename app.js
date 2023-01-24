586b406c44e362e1bc0fee2324d31a7872a81058
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-01-24 16:45:31 +0530

    Update : Added Question Data

---
+++ app.js
@@ -0,0 +1,58 @@
+const quizData = [
+    {
+        question : 'Which is the Most used languages in history ?',
+        a : 'Talokans',
+        b : 'Persian',
+        c : 'Afganisese',
+        d : 'Hindi',
+        correct : 'd'
+    }, 
+    {
+        question : 'What are the most used language in Programming ?',
+        a : 'Java',
+        b : 'Ruby', 
+        c : 'Javascript', 
+        d : 'Golang',
+        correct : 'c'
+    },
+    {
+        question : 'What are the most used tools in programming ?',
+        a : 'Vs-Code',
+        b : 'Open AI',
+        c : 'Power Shell',
+        d : 'CLI',
+        correct : 'c'
+    }, 
+    {
+        question : 'Who is the President of BCCI ?',
+        a : 'Narendra Modi', 
+        b : 'Saurav Ganguly', 
+        c : 'Mrinal Pramanick',
+        d : 'Roger Binny',
+        correct : 'd'
+    },
+    {
+        question : 'Dagar brothers are associated with which genre of Hindustani classical music?',
+        a : 'Khayal',
+        b : 'Dhrupad', 
+        c : 'Thumri', 
+        d : 'Tappa',
+        correct : 'b' 
+    }, 
+    {
+        question : 'Which dance is performed at the edge of the Brass Plate ?',
+        a : 'Bharatnatyam',
+        b : 'Kuchipudi',
+        c : 'Kathakali',
+        d : 'Manipuri',
+        correct : 'b'
+    }, 
+    {
+        question : 'Why schools are teaching "Python" to students ?',
+        a : 'Easy Syntax',
+        b : 'Easiest Language to write on paper',
+        c : 'No Complexity while solving questions', 
+        d : 'Easy to understand by all the children and students',
+        correct : 'd'
+    }
+]
