c9068723542c21984e930e93dbd7f715f12d4a4c
Mrinal <mrinalbitsat@gmail.com>
2023-05-10 19:31:32 +0530

    Added New Data

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,6 +2,7 @@
 
 import Input from "@/app/components/inputs/Input";
 import Button from "@/app/components/Button";
+import AuthSocialButton from "./AuthSocialButton"
 
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
@@ -80,11 +81,21 @@ const AuthForm = () => {
         </form>
 
         <div className="mt-6">
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-gray-300" />
-              </div>
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
             </div>
+
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with 
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton />
+          </div>
         </div>
       </div>
     </div>
---
+++ app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,12 @@
+import {IconType} from 'react-icons'
+
+const AuthSocialButton = () => {
+    return ( 
+        <div>
+            <h1>This is Social Buttons for Authentication</h1>
+            
+        </div>
+     );
+}
+ 
+export default AuthSocialButton;
