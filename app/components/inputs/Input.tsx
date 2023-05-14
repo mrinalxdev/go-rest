39dbc0b9e9c5cc2da71f4cb44a2854ec1999cd2e
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-05-14 17:07:22 +0530

    Added New Data

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,9 +1,32 @@
-const Input = () => {
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>       
-    )
+"use client";
+
+import clsx from "clsx";
+import { FieldValues, FieldErrors, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  label: string;
+  id: string;
+  type?: string;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  disabled: boolean;
 }
 
-export default Input
+const Input: React.FC<InputProps> = ({
+  label,
+  id,
+  type,
+  register,
+  required,
+  errors,
+  disabled,
+}) => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Input;
