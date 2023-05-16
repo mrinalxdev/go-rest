d5af8e1e522c2bd268b640c52762d5b72512607d
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-05-16 12:52:03 +0530

    Added Social Buttons for Authentication

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -4,6 +4,9 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Input from "@/app/components/inputs/Input";
 import Button from "@/app/components/Button";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub } from "react-icons/bs";
+import { BsGoogle } from "react-icons/bs";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -71,13 +74,47 @@ const AuthForm = () => {
           />
 
           <div>
-            <Button disabled={isLoading} fullWidth type="submit" >
-              {variant === 'LOGIN' ? 'Sign In' : 'Sign Up'}
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign In" : "Sign Up"}
             </Button>
           </div>
         </form>
 
-        
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or Continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-2 text-sm mt-6 px-2 justify-center text-gray-500">
+          <div>
+            {variant === "LOGIN"
+              ? "New to Messanger?"
+              : "Already have an account"}
+          </div>
+          <div
+            onClick={toggleVariant}
+            className="underline cursor-pointer"
+          ></div>
+        </div>
       </div>
     </div>
   );
