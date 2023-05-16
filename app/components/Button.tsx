6aca4254901dbe2556685dc6a7198dd108b9a6d2
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-05-16 11:04:08 +0530

    Working on Button.tsx

---
+++ app/components/Button.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import clsx from "clsx";
+
+interface ButtonProps {
+  type?: "button" | "submit" | "reset" | undefined;
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  secondary?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  type,
+  fullWidth,
+  children,
+  onClick,
+  secondary,
+  danger,
+  disabled,
+}) => {
+  return (
+    <button onClick={onClick} type={type} disabled={disabled}>
+
+    </button>
+  );
+};
+export default Button;
