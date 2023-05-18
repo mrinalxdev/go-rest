23c0b4a075f53d72b879f158d0517698b94bffd2
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-05-19 00:41:34 +0530

    Minor addons

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -38,7 +38,9 @@ const Input: React.FC<InputProps> = ({
           disabled={disabled}
           {...register(id, { required })}
           className={clsx(
-            "form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6", errors[id] && "focus:ring-rose-500", disabled && "opacity-50 cursor-default"
+            "form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6",
+            errors[id] && "focus:ring-rose-500",
+            disabled && "opacity-50 cursor-default"
           )}
         />
       </div>
