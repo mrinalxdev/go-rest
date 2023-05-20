aa29abdef2698fbc39048595244b85e88f04a0fb
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-05-20 23:13:14 +0530

    Added New Data

--- app/components/sidebar/DesktopItem.tsx
+++ app/components/sidebar/DesktopItem.tsx
@@ -26,9 +26,9 @@ const DesktopItem: React.FC<DesktopItemProps> = ({
 
   return (
     <li onClick={handleClick}>
-        <Link href={href}>
+        <Link href={href} className={clsx("group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100", active && 'bg-gray-100 text-black' )}>
             <Icon className="h-6 w-6 shrink-0" />
-            <span>{label}</span>
+            <span className="sr-only">{label}</span>
         </Link>
     </li>
   );
