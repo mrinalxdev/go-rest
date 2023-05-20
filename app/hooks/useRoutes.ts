c85ddc8b89f202968b3800e4dbbb3d16226e60be
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-05-20 13:42:09 +0530

    Added New Data

---
+++ app/hooks/useRoutes.ts
@@ -0,0 +1,21 @@
+import {  usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { HiChat } from "react-icons/hi";
+import { HiArrowLeftOnRectangle, HiUsers } from "react-icons/hi2";
+import { signOut } from "next-auth/react";
+import useConversation from "./useConversation";
+
+
+const useRoutes = () => {
+    const pathname = usePathname()
+    const { conversationId } = useConversation()
+
+    const routes = useMemo(() => [
+        {
+            label : 'Chat',
+            href : '/conversation',
+            icon : HiChat,
+            active : pathname === '/conversations' || !!conversationId
+        }
+    ] , [pathname, conversationId])
+}
