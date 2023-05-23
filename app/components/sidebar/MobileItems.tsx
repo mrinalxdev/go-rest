8792154784838fefd7fed49f5e19ccd4892c8797
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-05-23 22:17:47 +0530

    Added New Data

--- app/components/sidebar/MobileItems.tsx
+++ app/components/sidebar/MobileItems.tsx
@@ -12,7 +12,7 @@ interface MobileItemsProps {
 
 const MobileItems: React.FC<MobileItemsProps> = ({
   href,
-  icon,
+  icon: Icon,
   active,
   onClick,
 }) => {
@@ -21,7 +21,17 @@ const MobileItems: React.FC<MobileItemsProps> = ({
       return onClick();
     }
   };
-  return <div>Mobile Items</div>;
+  return (
+    <Link
+      href={href}
+      onClick={onClick}
+      className={clsx(
+        "group flex gap-x-3 text-sm leading-6 font-semibold w-full justify-center p-4 text-gray-500 hover:text-black hover:bg-gray-100", active && "bg-gray-100 text-black "
+      )}
+    >
+      <Icon className="h-6 w-6"/>
+    </Link>
+  );
 };
 
 export default MobileItems;
--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -2,15 +2,13 @@ import DesktopSidebar from "./DesktopSidebar";
 import MobileFooter from "./MobileFooter";
 
 async function Sidebar({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="h-full">
-            <DesktopSidebar />
-            <MobileFooter />
-            <main className="lg:pl-20 h-full">
-                {children}  
-            </main>
-        </div>
-    )
+  return (
+    <div className="h-full">
+      <DesktopSidebar />
+      <MobileFooter />
+      <main className="lg:pl-20 h-full">{children}</main>
+    </div>
+  );
 }
 
 export default Sidebar;
