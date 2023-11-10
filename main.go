9d359c42ba200cf993221309fbfe79cb23f0a97b
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-11-10 22:42:10 +0530

    Added A Get request

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"errors"
 )
 
 type todo struct {
 	ID        string `json:"id"`
-	Item      string `json:"title"`
+	Item      string `json:"item"`
 	Completed bool `json:"completed"`
 }
 
@@ -33,9 +33,33 @@ func addTodo (context*gin.Context) {
 	context.IndentedJSON(http.StatusCreated, newTodo)
 }
 
+
+func getTodo (context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoId(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message" : "Todo not found"})
+		return
+	}
+	
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+func getTodoId (id string) (*todo, error) {
+	for i, t := range todos {
+		if t.ID == id {
+			return &todos[i], nil
+		}
+	}
+
+	return nil, errors.New("todo not found")
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
+	router.GET("/todos/:id", getTodo)
 	router.POST("/todos/create", addTodo)
 	router.Run("localhost:9090")
 }
